f64aba0e201768ef8095aae4662e5515de4bf2e9
michele <michele.damico@gmail.com>
2018-09-22 19:03:28 +0200

    Live Code Starting Point

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 mod trait_obj;
-mod concrete;
-mod action_tuple;
-#[macro_use]
-mod compose;
 
 pub trait Action {
     fn do_action(&self);
@@ -25,54 +21,5 @@ fn main() {
     );
 
     t_obj.do_action();
-    println!("----------------------------------------");
-
-    //
-    //    println!("Wild Slice:");
-    //
-    //    let wild_slice = [action(Hello {}), action(World {})];
-    //
-    //    wild_slice.do_action();
-    //    println!("----------------------------------------");
-    //
-
-    println!("Concrete Impl:");
-
-    let c = concrete::ActionComposite::new(Hello {}, World {});
-
-    c.do_action();
-
-    println!("... compose by World {{}} again:");
-    c.compose(World {}).do_action();
-    println!("----------------------------------------");
-
-
-    println!("Wild Tuple:");
-    let wild_tuple = (Hello {}, World {});
-    wild_tuple.do_action();
-    println!("----------------------------------------");
-
-    println!("Composite tuple!");
-    use compose::Compose;
-
-    impl Compose for Hello {}
-    impl Compose for World {}
-
-    let wild_compose = Hello {}.compose(World {});
-    wild_compose.do_action();
-
-    println!("... compose by Hello {{}} again:");
-    wild_compose.compose(Hello {}).do_action();
-    println!();
-    println!("----------------------------------------");
-
-
-    println!("compose! macro:");
-    println!("{:?}", compose![Hello{}]);
-    println!("{:?}", compose![Hello{}, World {}]);
-    println!("{:?}", compose![Hello{}, World {}, Hello {}]);
-    println!("{:?}", compose![Hello{}, World {}, Hello {}, World {}]);
-
-    compose![Hello{}, World {}, Hello {}, World {}].do_action();
     println!("----------------------------------------");
 }
